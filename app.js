458aafdc959217b4840d9c8890284e7d645ed387
Maciej Lenartowicz <mcjlnrtwcz@gmail.com>
2020-08-16 12:31:25 +0200

    Filter predictions with common english words

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const phonewords = require("phonewords");
+const words = require("an-array-of-english-words");
 
 const app = express();
 app.use(cors());
 const port = 3001;
 
 app.get("/predictions/:number", (req, res) => {
-  res.send(phonewords.numbersToWords(req.params.number).slice(0, 100));
+  const predictions = phonewords.numbersToWords(req.params.number);
+  res.send(
+    predictions.filter((prediction) => words.includes(prediction.toLowerCase()))
+  );
 });
 
 app.listen(port, () => {
